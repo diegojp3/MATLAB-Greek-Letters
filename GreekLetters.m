4af7b93ca89652c46e07a15ce062813305705bb2
diegojp3 <39844116+diegojp3@users.noreply.github.com>
2022-10-20 17:55:38 -0700

    Create GreekLetters.m

---
+++ GreekLetters.m
@@ -0,0 +1,144 @@
+% CAPITALS -----------------------
+
+% Alpha
+ALPHA = char(hex2dec('0391'));
+
+% Beta
+BETA = char(hex2dec('0392'));
+
+% Gamma
+GAMMA = char(hex2dec('0393'));
+
+% Delta
+DELTA = char(hex2dec('0394'));
+
+% Epsilon
+EPSILON = char(hex2dec('0395'));
+
+% Zeta
+ZETA = char(hex2dec('0396'));
+
+% Eta
+ETA = char(hex2dec('0397'));
+
+% Theta
+THETA = char(hex2dec('0398'));
+
+% Iota
+IOTA = char(hex2dec('0399')); 
+
+% Kappa
+KAPPA = char(hex2dec('039A'));
+
+% Lambda
+LAMBDA = char(hex2dec('039B'));
+
+% Mu
+MU = char(hex2dec('039C'));
+
+% Nu
+NU = char(hex2dec('039D'));
+
+% Xi
+XI = char(hex2dec('039E'));
+
+% Omicron
+OMICRON = char(hex2dec('039F'));
+
+% Rho
+RHO = char(hex2dec('03A1')); 
+
+% Sigma
+SIGMA = char(hex2dec('03A3')); 
+
+% Tau
+TAU = char(hex2dec('03A4')); 
+
+% Upsilon
+UPSILON = char(hex2dec('03A5')); 
+
+% Phi
+PHI = char(hex2dec('03A6')); 
+
+% Chi
+CHI = char(hex2dec('03A7')); 
+
+% Psi
+PSI = char(hex2dec('03A8')); 
+
+% Omega
+OMEGA = char(hex2dec('03A9')); 
+
+% lowercase ----------------------
+
+% Alpha
+alpha = char(hex2dec('03B1'));
+
+% Beta
+beta = char(hex2dec('03B2'));
+
+% Gamma
+gamma = char(hex2dec('03B3'));
+
+% Delta
+delta = char(hex2dec('03B4'));
+
+% Epsilon
+epsilon = char(hex2dec('03B5'));
+
+% Zeta
+zeta = char(hex2dec('03B6'));
+
+% Eta
+eta = char(hex2dec('03B7'));
+
+% Theta
+theta = char(hex2dec('03B8'));
+
+% Iota
+iota = char(hex2dec('03B9')); 
+
+% Kappa
+kappa = char(hex2dec('03BA'));
+
+% Lambda
+lambda = char(hex2dec('03BB'));
+
+% Mu
+mu = char(hex2dec('03BC'));
+
+% Nu
+nu = char(hex2dec('03BD'));
+
+% Xi
+xi = char(hex2dec('03BE'));
+
+% Omicron
+omicron = char(hex2dec('03BF'));
+
+% Pi
+Pi = char(hex2dec('03C0'));
+
+% Rho
+rho = char(hex2dec('03C1')); 
+
+% Sigma
+sigma = char(hex2dec('03C2')); 
+
+% Tau
+tau = char(hex2dec('03C4')); 
+
+% Upsilon
+upsilon = char(hex2dec('03C5')); 
+
+% Phi
+phi = char(hex2dec('03C6')); 
+
+% Chi
+chi = char(hex2dec('03C7')); 
+
+% Psi
+psi = char(hex2dec('03C8')); 
+
+% Omega
+omega = char(hex2dec('03C9')); 
